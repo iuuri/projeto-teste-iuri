fedff254e5e0dc836fcc726b8231673293d471a3
Iuri Souza <iuri.souzasantos@gmail.com>
2024-10-12 12:39:47 -0300

    Teste do modo debugger

--- app.py
+++ app.py
@@ -1,1 +1,8 @@
-print('Olá, estamos no nosso primeiro projeto!')
+print('Olá')
+
+def calcular_preco_combo(pizza, refrigerante):
+    total = pizza + refrigerante
+    print(total)
+
+calcular_preco_combo(30, 20)
+print('Programa finalizado')
